e2e90ba4ae2ae0c0c9abbda9f1e8a0ad279b5157
Qiyi Shan <adls371@outlook.com>
2020-09-10 02:29:57 -0700

    add recipe name for test blueprint

--- test.lua
+++ test.lua
@@ -4,14 +4,14 @@ require("gui")
 
 function start_unit_tests(player_index)
     if test_all then
-        insert_test_blueprint(player_index)
+        insert_test_blueprint("coal-liquefaction",player_index)
     end
 end
 
-function insert_test_blueprint(player_index)
+function insert_test_blueprint(recipe_name, player_index)
     local graph = BlueprintGraph.new(player_index)
     --- @type OutputSpec[]
-    local output_specs = { { ingredient = "coal-liquefaction", unit = 1, crafting_speed = 10 } }
+    local output_specs = { { ingredient = recipe_name, unit = 1, crafting_speed = 10 } }
     graph:generate_graph_by_outputs(output_specs)
     graph:generate_blueprint()
 end
